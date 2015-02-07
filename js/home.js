5022f5e96397b699ace62621e1307b2091a27f64
Andrew Thomas <andrewmthomas87@gmail.com>
2015-02-07 11:48:24 -0700

    Attempt to manipulate ghost image

--- js/home.js
+++ js/home.js
@@ -1,6 +1,19 @@
 
+var types = ['tote', 'yellow-tote', 'scoring-platform', 'mid-step', 'bin', 'litter'];
+
+var images = [];
+for (i = 0; i < 6; i++) {
+	images[i] = $('<img src="images/' + types[i] + '.png">');
+	images[i].css('width', (0.0625 * $(window).width()) + 'px');
+}
+
 $(document).ready(function() {
-	$('section#palette div').on('dragstart', startDrag);
+	$('section#palette div').on('dragstart', function(event) {
+		event = event.originalEvent;
+		var $img = images[$('section#palette div').index(this)];
+		event.dataTransfer.setDragImage($img[0], $img.width() / 2, $img.height() / 2);
+		event.dataTransfer.setData('type', $(this).attr('id'));
+	});
 	$('section#main').on('dragover', function(event) {
 		event.originalEvent.preventDefault();
 	});
@@ -9,7 +22,7 @@ $(document).ready(function() {
 		var id = event.dataTransfer.getData('type');
 		var x = event.pageX - Math.floor(0.03125 * $(window).width());
 		var y = event.pageY - 10;
-		var stack = $('<div style="top: ' + y + 'px; left: ' + x + 'px;"><img src="images/' + id + '.png" class="' + id + '"></div>');
+		var stack = $('<div style="top: ' + y + 'px; left: ' + x + 'px;"><img src="images/' + id + '.png" class="' + id + '" draggable="false"></div>');
 		stack.appendTo(this);
 		stack.fadeIn('fast');
 		stack.find('img').on('dragstart', function(event) {
@@ -17,7 +30,3 @@ $(document).ready(function() {
 		});
 	});
 });
-
-function startDrag(event) {
-	event.originalEvent.dataTransfer.setData('type', $(this).attr('id') ? $(this).attr('id') : $(this).attr('class'));
-}
