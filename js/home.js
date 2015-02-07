577a8ed1c3f2bdd9a3f09aea5e7ba5e08f6615ba
Andrew Thomas <andrewmthomas87@gmail.com>
2015-02-06 22:40:11 -0700

    Added dragging functionality and many other things

--- js/home.js
+++ js/home.js
@@ -1,4 +1,18 @@
 
 $(document).ready(function() {
-
+	$('section#palette div').on('dragstart', function(event) {
+		event.originalEvent.dataTransfer.setData('id', event.target.id);
+	});
+	$('section#main').on('dragover', function(event) {
+		event.originalEvent.preventDefault();
+	});
+	$('section#main').on('drop', function(event) {
+		event = event.originalEvent;
+		var id = event.dataTransfer.getData('id');
+		var x = event.pageX - Math.floor(0.03125 * $(window).width());
+		var y = event.pageY - 10;
+		var stack = $('<div style="top: ' + y + 'px; left: ' + x + 'px;"><img src="images/' + id + '.png" class="' + id + '"></div>');
+		stack.appendTo(this);
+		stack.fadeIn('fast');
+	});
 });
