ad1a579c415fd104096a49d0b7082d9e3fa4a562
Andrew Thomas <andrewmthomas87@gmail.com>
2015-02-07 10:22:59 -0700

    Edited drag code

--- js/home.js
+++ js/home.js
@@ -1,18 +1,25 @@
 
 $(document).ready(function() {
 	$('section#palette div').on('dragstart', function(event) {
-		event.originalEvent.dataTransfer.setData('id', event.target.id);
+		startDrag(event.originalEvent, $(this).attr('id'));
 	});
 	$('section#main').on('dragover', function(event) {
 		event.originalEvent.preventDefault();
 	});
 	$('section#main').on('drop', function(event) {
 		event = event.originalEvent;
-		var id = event.dataTransfer.getData('id');
+		var id = event.dataTransfer.getData('type');
 		var x = event.pageX - Math.floor(0.03125 * $(window).width());
 		var y = event.pageY - 10;
 		var stack = $('<div style="top: ' + y + 'px; left: ' + x + 'px;"><img src="images/' + id + '.png" class="' + id + '"></div>');
 		stack.appendTo(this);
 		stack.fadeIn('fast');
+		stack.find('img').on('dragstart', function(event) {
+			startDrag(event.originalEvent, $(this).attr('class'));
+		});
 	});
 });
+
+function startDrag(event, type) {
+	event.dataTransfer.setData('type', type);
+}
