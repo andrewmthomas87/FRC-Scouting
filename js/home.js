d89328c53d753d69b46fcd02612270d401881d18
Andrew Thomas <andrewmthomas87@gmail.com>
2015-02-07 12:10:36 -0700

    Stack placement

--- js/home.js
+++ js/home.js
@@ -1,32 +1,22 @@
 
-var types = ['tote', 'yellow-tote', 'scoring-platform', 'mid-step', 'bin', 'litter'];
-
-var images = [];
-for (i = 0; i < 6; i++) {
-	images[i] = $('<img src="images/' + types[i] + '.png">');
-	images[i].css('width', (0.0625 * $(window).width()) + 'px');
-}
-
 $(document).ready(function() {
 	$('section#palette div').on('dragstart', function(event) {
 		event = event.originalEvent;
-		var $img = images[$('section#palette div').index(this)];
-		event.dataTransfer.setDragImage($img[0], $img.width() / 2, $img.height() / 2);
-		event.dataTransfer.setData('type', $(this).attr('id'));
+		event.dataTransfer.setData('type', $(this).attr('class'));
 	});
 	$('section#main').on('dragover', function(event) {
 		event.originalEvent.preventDefault();
 	});
 	$('section#main').on('drop', function(event) {
 		event = event.originalEvent;
-		var id = event.dataTransfer.getData('type');
-		var x = event.pageX - Math.floor(0.03125 * $(window).width());
-		var y = event.pageY - 10;
-		var stack = $('<div style="top: ' + y + 'px; left: ' + x + 'px;"><img src="images/' + id + '.png" class="' + id + '" draggable="false"></div>');
+		var type = event.dataTransfer.getData('type');
+		var spacer = $('<div class="spacer"></div>');
+		var stack = $('<div style="display: none" class="stack" draggable="false"><img src="images/' + type + '.png"></div>');
+		stack.on('drop', function(event) {
+			return false;
+		});
+		spacer.appendTo(this);
 		stack.appendTo(this);
 		stack.fadeIn('fast');
-		stack.find('img').on('dragstart', function(event) {
-			startDrag(event.originalEvent, $(this).attr('class'));
-		});
 	});
 });
